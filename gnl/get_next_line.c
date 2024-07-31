c029a2e1779fbf10d418de646aa11a3364370a4e
ken <kenken7272721@gmail.com>
2024-07-31 15:06:39 +0900

    Add files of Get_Next_Line program.

---
+++ gnl/get_next_line.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 15:59:41 by keishii           #+#    #+#             */
+/*   Updated: 2024/06/08 13:42:24 by keishii          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*fill_stash(int fd, char *stash);
+static char	*extract_line(char *stash);
+static char	*extract_new_stash(char *stash);
+static char	*free_and_null(void *ptr);
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+
+	line = NULL;
+	if (fd == -1 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(stash);
+		return (stash = NULL);
+	}
+	stash = fill_stash(fd, stash);
+	if (!stash || *stash == '\0')
+	{
+		free(stash);
+		return (stash = NULL);
+	}
+	line = extract_line(stash);
+	if (!line)
+	{
+		free(line);
+		return (free_and_null(stash));
+	}
+	stash = extract_new_stash(stash);
+	if (!stash)
+		return (free_and_null(line));
+	return (line);
+}
+
+static char	*fill_stash(int fd, char *stash)
+{
+	ssize_t	bytes_read;
+	char	buffer[BUFFER_SIZE + 1];
+
+	bytes_read = 1;
+	if (!stash)
+		stash = ft_strdup("");
+	if (!stash)
+		return (NULL);
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (NULL);
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin(stash, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (stash);
+}
+
+static char	*extract_line(char *stash)
+{
+	int		len;
+	char	*ret_line;
+
+	if (!stash)
+		return (NULL);
+	len = 0;
+	while (stash[len] && stash[len] != '\n')
+		len++;
+	if (stash[len] == '\n')
+		len++;
+	ret_line = (char *)malloc((len + 1) * sizeof(char));
+	if (!ret_line)
+		return (NULL);
+	ret_line[len] = '\0';
+	while (len--)
+		ret_line[len] = stash[len];
+	return (ret_line);
+}
+
+static char	*extract_new_stash(char *stash)
+{
+	char	*new_stash;
+	size_t	len;
+	size_t	i;
+
+	if (!stash)
+		return (NULL);
+	len = 0;
+	while (stash[len] && stash[len] != '\n')
+		len++;
+	if (stash[len] == '\n')
+		len++;
+	new_stash = (char *)malloc((ft_strlen(stash) - len + 1) * sizeof(char));
+	if (!new_stash)
+		return (free_and_null(stash));
+	i = 0;
+	while (stash[len + i])
+	{
+		new_stash[i] = stash[len + i];
+		i++;
+	}
+	new_stash[i] = '\0';
+	free(stash);
+	return (new_stash);
+}
+
+static char	*free_and_null(void *ptr)
+{
+	free(ptr);
+	return (NULL);
+}
---
+++ gnl/get_next_line_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 14:13:21 by keishii           #+#    #+#             */
+/*   Updated: 2024/06/08 13:30:11 by keishii          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char) c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	new_str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) \
+								* sizeof(char));
+	if (!new_str)
+	{
+		free((void *)s1);
+		return (NULL);
+	}
+	while (s1[i])
+		new_str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		new_str[j++] = s2[i++];
+	new_str[j] = '\0';
+	free((void *)s1);
+	return (new_str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char		*tmp;
+	int			i;
+	size_t		len;
+
+	len = ft_strlen(s1);
+	i = 0;
+	tmp = (char *)malloc((len + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	while (s1[i])
+	{
+		tmp[i] = s1[i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
