676be21d949ce3ad00bd361ac7fdb04c6f485c4b
Ken Ishii <kenken7272721@gmail.com>
2024-08-03 16:28:44 +0900

    Disable destructor(). Not working properly.

--- src/main.c
+++ src/main.c
@@ -6,17 +6,16 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 13:50:31 by keishii           #+#    #+#             */
-/*   Updated: 2024/08/03 14:13:07 by keishii          ###   ########.fr       */
+/*   Updated: 2024/08/03 15:59:06 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-__attribute__((destructor))
-static void	destructor(void)
-{
-	system("leaks -q so_long");
-}
+// __attribute__((destructor))
+// static void destructor() {
+//     system("leaks -q so_long");
+// }
 
 int	main(int argc, char **argv)
 {
