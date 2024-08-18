d4de76487709c18b6862a523bb1cd62f517083a7
Ken Ishii <kenken7272721@gmail.com>
2024-08-18 16:30:44 +0900

    Rename maps directory to 'maps'

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 18:41:50 by keishii           #+#    #+#             */
-/*   Updated: 2024/08/18 13:57:45 by keishii          ###   ########.fr       */
+/*   Updated: 2024/08/18 16:27:14 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include <math.h>
-
-# include <stdio.h>
 
 # include "mlx.h"
 # include "ft_printf.h"
